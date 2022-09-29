904c13e3815dcb73cb28371a644880fac7e3107c
Sebastiaan Eekhof <sebastiaan@eekhof.dev>
2022-09-29 14:45:14 +0200

    test

---
+++ App.php
@@ -0,0 +1,19 @@
+<?php
+class App {
+    public function get() {
+
+    }
+
+    public function post() {
+
+    }
+
+    public function put() {
+
+    }
+
+    public function delete() {
+        
+    }
+}
+?>
